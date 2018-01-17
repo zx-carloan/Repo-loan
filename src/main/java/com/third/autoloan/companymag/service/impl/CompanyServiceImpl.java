437b400d51bc929b6709c6e3692ae7d8f0ad1efa
xzl <xzl>
2018-01-17 21:33:10 +0800

    2.20 新增公司的查询方法

--- src/main/java/com/third/autoloan/companymag/service/impl/CompanyServiceImpl.java
+++ src/main/java/com/third/autoloan/companymag/service/impl/CompanyServiceImpl.java
@@ -1,16 +1,19 @@
 package com.third.autoloan.companymag.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.third.autoloan.beans.CompanyBean;
 import com.third.autoloan.companymag.repository.CompanyRepository;
+import com.third.autoloan.companymag.service.ICompanyGetService;
 import com.third.autoloan.companymag.service.ICompanyService;
 
 
 @Service("companyServiceImpl")
-public class CompanyServiceImpl implements ICompanyService {
+public class CompanyServiceImpl implements ICompanyService , ICompanyGetService {
 
 	@Resource
 	private CompanyRepository companyRepository;
@@ -20,6 +23,12 @@ public class CompanyServiceImpl implements ICompanyService {
 		// TODO Auto-generated method stub
 
 		companyRepository.save(company);
+	}
+
+	@Override
+	public List<CompanyBean> listCompanyInfo() {
+	
+		return 	companyRepository.findAll();
 	}
 
 }
--- src/main/java/com/third/autoloan/companymag/service/impl/ProductServiceImpl.java
+++ src/main/java/com/third/autoloan/companymag/service/impl/ProductServiceImpl.java
@@ -1,15 +1,18 @@
 package com.third.autoloan.companymag.service.impl;
 
+import java.util.List;
+
 import javax.annotation.Resource;
 
 import org.springframework.stereotype.Service;
 
 import com.third.autoloan.beans.ProductBean;
 import com.third.autoloan.companymag.repository.ProductRepository;
+import com.third.autoloan.companymag.service.IProductGetService;
 import com.third.autoloan.companymag.service.IProductService;
 
 @Service("productServiceImpl")
-public class ProductServiceImpl implements IProductService {
+public class ProductServiceImpl implements IProductService,IProductGetService {
 
 	@Resource
 	private ProductRepository productRepository;
@@ -18,6 +21,12 @@ public class ProductServiceImpl implements IProductService {
 	public void saveProduct(ProductBean product) {
 		// TODO Auto-generated method stub
 		productRepository.save(product);
+	}
+
+	@Override
+	public List<ProductBean> listProductInfo() {
+		// TODO Auto-generated method stub
+		return productRepository.findAll();
 	}
 
 }
