7216c755fe5915a6f3ca24b1d244da1d4c66c794
xzl <xzl>
2018-01-17 08:34:35 +0800

    1.1

--- src/main/java/com/third/autoloan/beans/ContractBean.java
+++ src/main/java/com/third/autoloan/beans/ContractBean.java
@@ -7,6 +7,7 @@ import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.FetchType;
 import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToOne;
 import javax.persistence.Table;
@@ -25,8 +26,7 @@ public class ContractBean implements Serializable {
 	private static final long serialVersionUID = 5677637847066848501L;
 	@Id
 	@Column(name="id")
-	@GenericGenerator(name="id.strategy", strategy="identity")
-	@GeneratedValue(generator="id.strategy")
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private long id;
 	@Column
 	private double amount;//审批金额
