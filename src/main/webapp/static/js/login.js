4535e719cc61024e8d3ae191b4867208d796d080
Administrator <Administrator@PC201709142055>
2018-01-22 12:42:09 +0800

    g2.0添加验证码

--- src/main/webapp/static/js/login.js
+++ src/main/webapp/static/js/login.js
@@ -1,30 +1,57 @@
-	function login() {
+function changeImg() {
+    var imgSrc = $("#codeImg");
+    var src = imgSrc.attr("src");
+    imgSrc.attr("src", chgUrl(src));
+}  
+
+function chgUrl(url) {
+    var timestamp = (new Date()).valueOf();if ((url.indexOf("&") >= 0)) {
+        url = url + "&timestamp=" + timestamp;
+    } else {
+        url = url + "?timestamp=" + timestamp;
+    }
+    return url;
+}
+	
+function login() {
+	var loginCode = $('#loginCode').val();
+	if(loginCode ==''||loginCode==null){
+		$('#yanzheng').text("请输入验证码！");
+		changeImg();
+	}else{
 		$.ajax({
-			url : '/task/usermag/login',
+			url : '/task/usermag/'+loginCode,
 			type : 'POST', 
 			async : true, 
 			data : datas(),
 			dataType:'text',
 			success : function(msg) {
-				if(msg == 'err'){
-					$("#nameT").text("用户或者密码错误");
+				if(msg == 'err'){	
+					$("#nameT").text("用户或密码错误");
 					clear();
+					changeImg();
 				}else {
 					$(location).attr('href', msg);
 				}
 			},
 		}) 
 	}
+}
 
-	function datas(){
-		var loginName =  $('#name').val();
-		var  password=$('#pwd').val();
-		var data ={loginName:loginName,password:password};
-		return data;
-	}
+function datas(){
+	var loginName =  $('#name').val();
+	var  password=$('#pwd').val();
+	var data ={loginName:loginName,password:password};
+	return data;
+}
 
+function clear(){
+	$('#name').val('');
+	$('#pwd').val('');
+}
 
-	function clear(){
-		$('#name').val('');
-		$('#pwd').val('');
-	}
+	
+	
+
+	
+	
