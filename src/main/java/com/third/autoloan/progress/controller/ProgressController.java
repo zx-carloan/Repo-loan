30f6d8e87e71214db0833162e7da61bf4438eb0c
Administrator <Administrator@PC201703021517>
2018-01-23 11:20:54 +0800

    5.77

--- src/main/java/com/third/autoloan/progress/controller/ProgressController.java
+++ src/main/java/com/third/autoloan/progress/controller/ProgressController.java
@@ -32,7 +32,7 @@ public class ProgressController {
 	private IProgressService progressService;
 	
 	//客户信息信息
-	@Resource(name = "creditGetServiceImpl")
+	@Resource(name = "creditGetServiceImpl") 
 	private ICreditGetService creditGetService;
 
 	// 查询全部信息
